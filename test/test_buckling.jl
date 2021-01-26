e63ab387370e36fb957c84abe6b61deb92fdb7ea
Yijiang Huang <yijiangh@mit.edu>
2021-01-25 20:51:44 -0500

    [WIP] viz buckling modes

--- test/test_buckling.jl
+++ test/test_buckling.jl
@@ -16,7 +16,7 @@ problem_json = ["buckling_2d_global_instab.json", "buckling_2d_nodal_instab.json
 ins_dir = joinpath(@__DIR__, "instances", "fea_examples");
 
 # @testset "Buckling problem solve - $(problem_json[i])" for i in 1:length(problem_json)
-    i = 2
+    i = 1
     file_name = problem_json[i]
     problem_file = joinpath(ins_dir, file_name)
 
@@ -38,54 +38,72 @@ ins_dir = joinpath(@__DIR__, "instances", "fea_examples");
     @test getgeomorder(problem) == 1
     @test nnodespercell(problem) == 2
 
-    # quad_order=default_quad_order(problem)
-    # elementinfo = ElementFEAInfo(problem, quad_order, Val{:Static});
     solver = FEASolver(Displacement, Direct, problem)
-    ## call solver to trigger assemble!
+    # call solver to trigger assemble!
+    solver()
+    @show solver.u
+    # scene, layout = draw_truss_problem(problem; u=solver.u)
+
     try
-        solver()
-        @show solver.u
-        # scene, layout = draw_truss_problem(problem; u=solver.u)
+        global K, Kσ = buckling(problem, solver.globalinfo, solver.elementinfo)
+    catch err
+        println(RED_BG("ERROR: "))
+        println(err)
+        if isa(err, SingularException)
+            println(RED_FG("Linear elasticity solve failed."))
+            K_l = solver.globalinfo.K
+            # TODO: use sparse eigen
+            # λ, ϕ = eigs(K.data, nev = 2, which=:SM);
+            # @show λ
+            # @show ϕ
 
-        K, Kσ = buckling(problem, solver.globalinfo, solver.elementinfo)
-        println(GREEN_FG("Linear elasticity solved."))
+            # ? is this ordered by eigen values' magnitude?
+            F = eigen(Array(K_l))
+            @show F
+            # @assert abs(F.values[1] - 0.0) < eps()
 
-        # Find the maximum eigenvalue of system Kσ x = 1/λ K x
-        # https://julialinearalgebra.github.io/IterativeSolvers.jl/stable/eigenproblems/lobpcg/
-        r = lobpcg(Kσ.data, K.data, true, 1)
+            # * draw eigen mode
+            # scene, layout = draw_truss_problem(problem; u=F.vectors[:,1])
+        end
+    end
+    println(GREEN_FG("Linear elasticity solved."))
 
-        # Minimum eigenvalue of the system K x = λ Kσ x
-        @show λ = 1/r.λ[1]
+    # Find the maximum eigenvalue of system Kσ x = 1/λ K x
+    # https://julialinearalgebra.github.io/IterativeSolvers.jl/stable/eigenproblems/lobpcg/
+    r = lobpcg(Kσ.data, K.data, true, 1)
 
-        s = 1.0
-        Ks = K .+ s .* Kσ
-        cholKs = cholesky(Symmetric(Ks))
-        u_b = cholKs \ s .* solver.globalinfo.f
-        println(GREEN_FG("Buckling problem solved."))
+    # # Minimum eigenvalue of the system K x = λ Kσ x
+    @show λ = 1/r.λ[1]
+
+    F = eigen(Array(Kσ), Array(K))
+    # F = eigen(Array(K), Array(Kσ))
+    @show 1 ./ F.values
+
+    # if 0 < λ && λ < 1
+        # 0 < λ_min < 1 means that the equilibrium equation with load 
+        # λ*f is not solvable for some 0 < λ < 1 and the structure under load
+        # f is not stable
+
+        # s = 1.0
+        # Ks = K .+ λ .* Kσ
+
+        # eigen_vals, eigen_vecs = eigs(K, nev = 2, which=:SM);
+        # @show eigen_vals
+        # @show eigen_vecs
+        # F = eigen(Array(K))
+        # @show F
+
+        # cholKs = cholesky(Symmetric(Ks))
+        # u_b = cholKs \ solver.globalinfo.f
+        # println(GREEN_FG("Buckling problem solved."))
 
         ## TODO plot analysis result with
-        # scene, layout = draw_truss_problem(problem; u=solver.u)
-        scene, layout = draw_truss_problem(problem; u=u_b)
-    catch err
-        println(RED_FG("Linear elasticity failed."))
-        println(err)
-        K = solver.globalinfo.K
-        # TODO: use sparse eigen
-        # λ, ϕ = eigs(K.data, nev = 2, which=:SM);
-        # @show λ
-        # @show ϕ
-
-        # ? is this ordered by eigen values' magnitude?
-        F = eigen(Array(K))
-        # @show F
-        @assert abs(F.values[1] - 0.0) < eps()
-
-        # * draw eigen mode
-        scene, layout = draw_truss_problem(problem; u=F.vectors[:,1])
-    end
+        # scene, layout = draw_truss_problem(problem; u=u_b)
+        # println("End")
+    # end
 
     # TODO eigenmode to show global instability mode
-    # TODO finite difference to verify the gradientj
+    # TODO finite difference to verify the gradient
     # TODO verify the gradient for some analytical problems
     # TODO "manual" interior point loop, adjusting the c value every iter
 
